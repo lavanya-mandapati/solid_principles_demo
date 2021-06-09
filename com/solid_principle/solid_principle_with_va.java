e91487a751460cf8cb0914ce36af45de0b6e3164
Lavanya <lavanyareddy818683@gmail.com>
2021-06-09 16:22:13 +0530

    demo_first_commit

--- com/solid_principle/solid_principle_with_va.java
+++ com/solid_principle/solid_principle_with_va.java
@@ -1,6 +1,6 @@
 package com.solid_principle;
 import com.solid_principle.sub.solid_principle_orient_sub;
-public class solid_principle_with_va extends solid_principle_without_va{
+public class solid_principle_with_va extends solid_principle_orient{
     @Override
     public void temperature_detector() {
         super.temperature_detector();
